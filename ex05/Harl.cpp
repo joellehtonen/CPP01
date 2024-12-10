79f6c559a2f8769aa39ef5d28ac60faf6cff6522
Joel Lehtonen <jlehtone@c3r5p3.hive.fi>
2024-12-10 15:37:18 +0200

    added more fun messages

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -2,38 +2,39 @@
 
 void Harl::debug(void)
 {
-	std::cout << "THIS IS A DEBUG MESSAGE" << std::endl;
+	std::cout << "this is a mild debug message" << std::endl;
+	std::cout << "it likes food without spices or condiments, and its drink of choice is room temperature tap water without added minerals" << std::endl;
 }
 void	Harl::info(void)
 {
-	std::cout << "THIS IS AN INFO MESSAGE" << std::endl;
+	std::cout << "This is a moderate and polite info message." << std::endl;
+	std::cout << "It dresses in business casual Mon-Fri, and prefers to vote for center-left or center-right." << std::endl;
 };
 void	Harl::warning(void)
 {
-	std::cout << "THIS IS A WARNING MESSAGE" << std::endl;;
+	std::cout << "This Is a Serious Warning Message" << std::endl;
+	std::cout << "Its Handshake Is Firm Enough to Hurt, And When Laughing It Exhales Through His Nose for Exactly 1 Second to Save Time for More Important Matters." << std::endl;
 };
 void	Harl::error(void)
 {
-	std::cout << "THIS IS AN ERROR MESSAGE" << std::endl;;
+	std::cout << "THIS IS AN EXTREMELY IMPORTANT ERROR MESSAGE THAT YOU CANNOT IGNORE!!!" << std::endl;
+	std::cout << "LOOK AT ME! DON'T FUCKING IGNORE ME! READ ME ALL THE WAY THROUGH! ALL THE WAY! KEEP FUCKING GOING! CLASS DISMISSED, AND BE BETTER NEXT TIME!" << std::endl;
 };
 
 void	Harl::complain(std::string level)
 {
-	void (Harl::*ptr)(void) = nullptr;
+	void (Harl::*functionPointers[])(void) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string complaints[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
-	if (level == "DEBUG")
-		ptr = &Harl::debug;
-	else if (level == "INFO")
-		ptr = &Harl::info;
-	else if (level == "WARNING")
-		ptr = &Harl::warning;
-	else if (level == "ERROR")
-		ptr = &Harl::error;
-	else
+	for (int i = 0; i < 4; i++)
 	{
-		std::cerr << "Error. Not a valid level" << std::endl;
-		std::cout << "Acceptable levels are: DEBUG / INFO / WARNING / ERROR" << std::endl;
-		return ;
+		if (complaints[i] == level)
+		{
+			(this->*functionPointers[i])();
+			return ;
+		}
 	}
-	(this->*ptr)();
+	std::cerr << "Error. Not a valid level" << std::endl;
+	std::cout << "Acceptable levels are: DEBUG / INFO / WARNING / ERROR" << std::endl;
+	return ;
 };
