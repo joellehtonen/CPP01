ef0d3f31cd48546c3d48a9ddcebf4cd40b2db6dd
Joel Lehtonen <jlehtone@c2r5p3.hive.fi>
2025-01-02 16:34:18 +0200

    removed comments

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -16,9 +16,9 @@ int main(void)
         Weapon club = Weapon("crude spiked club");
 
         HumanB bob("Bob");
-        //bob.setWeapon(club);
+        bob.setWeapon(club);
         bob.attack();
         club.setType("some other type of club");
-        //bob.attack();
+        bob.attack();
     }
 }
